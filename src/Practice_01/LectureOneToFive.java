d81a4bf34e22927d127034dac0e676b82c64ea84
fmhasan25 <fmhasan25@gmail.com>
2020-09-09 22:24:09 -0400

    Change 2

--- src/Practice_01/LectureOneToFive.java
+++ src/Practice_01/LectureOneToFive.java
@@ -3,11 +3,9 @@ package Practice_01;
 public class LectureOneToFive {
     public static void main(String[] args) {
         //how are you
+        System.out.println("Hello World");
 
 
     }
 
 }
-
-
-
